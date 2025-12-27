0767e3954e846d68bff48f7f5de4edb11cfee719
abiola1864 <oyebanjoabiola@gmail.com>
2025-12-27 16:33:41 +0100

    improved css

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@
 // FIXED: Removed Font Awesome from cache
 // ===============================================
 // ===== SERVICE WORKER FOR OFFLINE SUPPORT =====
-const ONE_DAY_MS = 24 * 60 * 60 * 2590;
+const ONE_DAY_MS = 24 * 60 * 60 * 2190;
 const CACHE_VERSION = `v-${Date.now() + ONE_DAY_MS}`;
 const CACHE_NAME = `weather-game-${CACHE_VERSION}`;
 
