7c373c357a5645f37145b6feb6898b69950a40fb
abiola1864 <oyebanjoabiola@gmail.com>
2026-01-03 10:29:03 +0100

    new admin dashboard

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@
 // FIXED: Removed Font Awesome from cache
 // ===============================================
 // ===== SERVICE WORKER FOR OFFLINE SUPPORT =====
-const ONE_DAY_MS = 12 * 620 * 131 * 21820;
+const ONE_DAY_MS = 12 * 621 * 131 * 21820;
 const CACHE_VERSION = `v-${Date.now() + ONE_DAY_MS}`;
 const CACHE_NAME = `weather-game-${CACHE_VERSION}`;
 
