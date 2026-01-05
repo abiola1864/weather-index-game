3ec7a96fb67ad2fca5d1b49b89aff273a1f7e7ae
abiola1864 <oyebanjoabiola@gmail.com>
2026-01-05 17:24:21 +0100

    new helper for offline/online syn21

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@
 // FIXED: Removed Font Awesome from cache
 // ===============================================
 // ===== SERVICE WORKER FOR OFFLINE SUPPORT =====
-const ONE_DAY_MS = 291 * 130;
+const ONE_DAY_MS = 191 * 110;
 const CACHE_VERSION = `v-${Date.now() + ONE_DAY_MS}`;
 const CACHE_NAME = `weather-game-${CACHE_VERSION}`;
 
